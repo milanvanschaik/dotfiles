de7b9d79ebde56ce001bd2bf5c3ac8d3dfbfa615
Milan van Schaik <info@milanvanschaik.nl>
2017-05-31 14:12:43 +0200

    Initial stuff

---
+++ macos/dock.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+dockutil --no-restart --remove all
+dockutil --no-restart --add "/Applications/Google Chrome.app"
+dockutil --no-restart --add "/Applications/Utilities/Terminal.app"
+dockutil --no-restart --add "/Applications/Spotify.app"
+
+killall Dock
